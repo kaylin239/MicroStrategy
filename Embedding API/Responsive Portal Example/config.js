b80cc50711f2d2dda8f2850d3960b0e33a7953ca
Stephen Lippens <stephen.lippens@gmail.com>
2019-04-15 13:01:53 -0400

    new

--- Embedding API/Responsive Portal Example/config.js
+++ Embedding API/Responsive Portal Example/config.js
@@ -3,8 +3,8 @@
 config = {
 	"embeddingLibPath": "https://env-127948.customer.cloud.microstrategy.com/MicroStrategyLibrary/javascript/embeddinglib.js",
 	"baseRestURL": "https://env-127948.customer.cloud.microstrategy.com/MicroStrategyLibrary",
-    "username": "steve",
-    "password": "",
+    "username": "MyUser",
+    "password": "MyPass",
     "projectID": "B7CA92F04B9FAE8D941C3E9B7E0CD754",
     "dossierID": "A41EA62A11E95095D1400080EF25BFEE"
 }
