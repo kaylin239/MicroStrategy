ea7f2fb13b16f633d120e8caeb423f2da2a59b29
Stephen Lippens <stephen.lippens@gmail.com>
2019-04-15 12:40:56 -0400

    upload

---
+++ Embedding API/Responsive Portal Example/config.js
@@ -0,0 +1,10 @@
+
+//Configure the below variables based on your environment
+config = {
+	"embeddingLibPath": "https://env-127948.customer.cloud.microstrategy.com/MicroStrategyLibrary/javascript/embeddinglib.js",
+	"baseRestURL": "https://env-127948.customer.cloud.microstrategy.com/MicroStrategyLibrary",
+    "username": "steve",
+    "password": "",
+    "projectID": "B7CA92F04B9FAE8D941C3E9B7E0CD754",
+    "dossierID": "A41EA62A11E95095D1400080EF25BFEE"
+}
