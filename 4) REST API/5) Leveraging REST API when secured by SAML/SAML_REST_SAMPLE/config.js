1120d24a6e2817e9787bed98e3b1578cf28b8287
Stephen Lippens <stephen.lippens@gmail.com>
2019-08-05 12:04:55 -0400

    new

---
+++ 4) REST API/5) Leveraging REST API when secured by SAML/SAML_REST_SAMPLE/config.js
@@ -0,0 +1,8 @@
+
+//Configure the below variables based on your environment
+config = {
+	"embeddingLibPath": "https://env-147146.customer.cloud.microstrategy.com/MicroStrategyLibrary/javascript/embeddinglib.js",
+	"baseRestURL": "https://env-147146.customer.cloud.microstrategy.com/MicroStrategyLibrary",
+    "projectID": "B7CA92F04B9FAE8D941C3E9B7E0CD754",
+    "dossierID": "078DBE5811E97715F6E10080EF156016"
+}
